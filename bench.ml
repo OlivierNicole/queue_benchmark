3846f9fba9dc67269eff9cb1ed159f38a5a3a551
Olivier Nicole <olivier@chnik.fr>
2022-02-09 10:29:13 +0100

    Try adding padding, improve bench

--- bench.ml
+++ bench.ml
@@ -13,13 +13,6 @@ let my_fprintf out =
   end
 let my_printf fmt = my_fprintf stdout fmt
 
-(*! module Queue = UnboundedQueue !*)
-(*module Queue = UnboundedQueue2*)
-module Queue = Queue.BufferQueueAtomArrOpt
-
-
-(** Functional correctness test *)
-
 let nmax = 1_200_000
 let nb_consumers = 1
 let nb_per_consumer =
@@ -29,140 +22,77 @@ let nb_producers = 1
 let nb_per_producer =
   assert (nmax mod nb_producers = 0) ;
   nmax / nb_producers
-let channel = Queue.make ~capacity:8192 ~dummy:42
+let capacity = 16_384
 
-let producer i =
-  let init = i*nb_per_producer in
-  for n = init+1 to init+nb_per_producer do
-    (*my_printf "enqueue %i…\n" n ;*)
-    Queue.enqueue channel n ;
-    (*my_printf "enqueued\n" ;*)
-  done
+module Bench (Q : Queue.QUEUE) = struct
+  let channel = Q.make ~capacity ~dummy:42
 
-let consumer () =
-  let sum = ref 0 in
-  for _ = 1 to nb_per_consumer do
-    (*my_printf "    dequeue…\n" ;*)
-    let n = Queue.dequeue channel in
-    sum := !sum + n ;
-    (*my_printf "    dequeued %i\n" n ;*)
-  done ;
-  !sum
-
-let f () =
-  let sum =
-    run_n (nb_producers + nb_consumers)
-      ~thread:(fun tid -> if tid < nb_producers then (producer tid ; 0) else consumer ())
-      ~init:0
-      ~collect:(+)
-  in
-  (*my_printf "sum = %i\n" sum ;*)
-  assert (sum = nmax * (nmax + 1) / 2) ;
-  (*my_printf "done\n"*)
-  ()
-
-let () =
-  let nruns = 100 in
-  let t0 = Unix.gettimeofday () in
-  for _ = 1 to nruns do
-    f ()
-  done;
-  let t1 = Unix.gettimeofday () in
-  Printf.printf "#Producers: %i\n#Consumers: %i\n" nb_producers nb_consumers;
-  (*Printf.printf "Total %i runs: %f s\n" nruns (t1 -. t0);*)
-  Printf.printf "Average time: %f ms\n" ((t1 -. t0) /. float_of_int nruns *. 1000.)
-
-let () =
-  exit 0
-
-
-(*
-(** Performance test *)
-
-let nmax = 100_000
-let nb_consumers = 10
-let channel = Queue.make ~capacity:4 ~dummy:(42,None)
-
-let producer () =
-  let primes = ref [] in
-  for n = 1 to nmax do
-    let elt =
-      begin match List.find (fun p -> n mod p = 0) !primes with
-      | p                   -> (n, Some p)
-      | exception Not_found -> if n <> 1 then primes := n :: !primes; (n, None)
-      end
-    in
-    Queue.enqueue channel elt
-  done
-
-let consumer () =
-  let primes = ref [] in
-  for _ = 1 to nmax / nb_consumers do
-    let (n, opt_p) = Queue.dequeue channel in
-    (*let s =
-      begin match opt_p with
-      | None   -> Printf.sprintf "%u is prime\n" n
-      | Some p -> Printf.sprintf "%u is dividible by %u\n" n p
-      end
-    in
-    Printf.printf "%s" s*)
-    begin match opt_p with
-    | None   -> primes := n :: !primes
-    | Some _ -> ()
-    end
-  done ;
-  List.rev !primes
-
-(*
-let () =
-  let finished = Atomic.make false in
-  Domain.spawn begin fun () ->
-    while not @@ Atomic.get finished do
-      Unix.sleep 1 ;
-      Printf.printf "%!" ;
+  let producer i =
+    let init = i*nb_per_producer in
+    for n = init+1 to init+nb_per_producer do
+      (*my_printf "enqueue %i…\n" n ;*)
+      Q.enqueue channel n ;
+      (*my_printf "enqueued\n" ;*)
     done
-  end ;
-  run_n (1 + nb_consumers)
-    ~thread:(fun tid -> if tid = 0 then producer () else consumer ())
-    ~init:()
-    ~collect:(fun () () -> ()) ;
-  Atomic.set finished true ;
-  Printf.printf "done\n%!"
-*)
 
-let f () =
-  (*
-  let finished = Atomic.make false in
-  ignore @@ Domain.spawn begin fun () ->
-    while not @@ Atomic.get finished do
-      Unix.sleep 1 ;
-      Printf.printf "%!" ;
-    done
-  end ;
-  *)
-  let primes =
-    run_n (1 + nb_consumers)
-      ~thread:(fun tid -> if tid = 0 then (producer () ; []) else consumer ())
-      ~init:[]
-      ~collect:(@)
-  in
-  ignore (Sys.opaque_identity primes) ;
-  (*
-  Printf.printf "[";
-  List.iter (fun i -> Printf.printf "%i, " i) primes;
-  Printf.printf "]\n";
-  *)
-  (*Atomic.set finished true ;*)
-  (*Printf.printf "done\n%!"*)
-  ()
+  let consumer () =
+    let sum = ref 0 in
+    for _ = 1 to nb_per_consumer do
+      (*my_printf "    dequeue…\n" ;*)
+      let n = Q.dequeue channel in
+      sum := !sum + n ;
+      (*my_printf "    dequeued %i\n" n ;*)
+    done ;
+    !sum
+
+  let f () =
+    let sum =
+      run_n (nb_producers + nb_consumers)
+        ~thread:(fun tid -> if tid < nb_producers then (producer tid ; 0) else consumer ())
+        ~init:0
+        ~collect:(+)
+    in
+    (*my_printf "sum = %i\n" sum ;*)
+    assert (sum = nmax * (nmax + 1) / 2) ;
+    (*my_printf "done\n"*)
+    ()
+
+  let bench ~name =
+    let nruns = 100 in
+    Printf.printf "=== %s ===\n" name;
+    Printf.printf "#Producers: %i\n#Consumers: %i\nCapacity: %i\n%!" nb_producers
+      nb_consumers capacity;
+    let t0 = Unix.gettimeofday () in
+    for _ = 1 to nruns do
+      f ()
+    done;
+    let t1 = Unix.gettimeofday () in
+    (*Printf.printf "Total %i runs: %f s\n" nruns (t1 -. t0);*)
+    Printf.printf "Average time: %f ms\n\n" ((t1 -. t0) /. float_of_int nruns *. 1000.)
+end
+
+module B1 = Bench (Queue.BufferQueue)
+module B2 = Bench (Queue.BufferQueueArrAtom)
+module B3 = Bench (Queue.BufferQueueAtomArr)
+module B4 = Bench (Queue.BufferQueueAtomArrOpt)
+
+let orig = ref false
+let arr_atom = ref false
+let atom_arr = ref false
+let atom_arr_opt = ref false
+
+let speclist =
+  [ ("-orig", Arg.Set orig, "Original queue implementation");
+    ("-arratom", Arg.Set arr_atom, "Array of atomics");
+    ("-atomarr", Arg.Set atom_arr, "Atomic arrays");
+    ("-atomarropt", Arg.Set atom_arr_opt, "Atomic arrays (padded)");
+  ]
 
 let () =
-  let nruns = 1 in
-  let t0 = Unix.gettimeofday () in
-  for _ = 0 to nruns do
-    f ()
-  done;
-  let t1 = Unix.gettimeofday () in
-  Printf.printf "Total %i runs: %f s\n" nruns (t1 -. t0);
-  Printf.printf "Average: %f s\n" ((t1 -. t0) /. float_of_int nruns);
-*)
+  Arg.parse speclist
+    (fun _ -> raise (Invalid_argument "anonymous argument not supported"))
+    "bench [-orig] [-arratom] [-atomarr] [-atomarropt]";
+  (if !atom_arr_opt then B4.bench ~name:"Atomic array (padded)");
+  (if !orig then B1.bench ~name:"Original");
+  (if !arr_atom then B2.bench ~name:"Array of atomics");
+  (if !atom_arr then B3.bench ~name:"Atomic array");
--- queue.ml
+++ queue.ml
@@ -403,8 +403,6 @@ module BufferQueueAtomArrOpt : QUEUE = struct
       mask : int ;
       head : int Atomic.t ;
       tail : int Atomic.t ;
-      blockshift : int ;
-      blockmask : int ;
     }
 
   (* Default recommended capacity: 64 *)
@@ -413,19 +411,13 @@ module BufferQueueAtomArrOpt : QUEUE = struct
     (* check that the capacity is a power of two (not required by the algorithm,
      * but it allows more efficient array lookups): *)
     assert (capacity land (capacity - 1) = 0) ;
-    (* Check that the capacity is a multiple of 8 (required to do index
-       remapping in order to avoid cache line invalidation by adjacent accesses.
-       *)
-    assert (capacity mod 8 = 0) ;
-    let blocksize = capacity / 8 in (* [blocksize] is a power of 2 as well *)
     {
-      timestamps = Array.init capacity (fun i -> blocksize * (i mod 8) + i / 8) ;
-      elements = Array.make capacity dummy ;
+      timestamps = Array.init (8*capacity)
+        (fun i -> if i mod 8 = 0 then i / 8 else 42) ;
+      elements = Array.make (8*capacity) dummy ;
       mask = capacity - 1 ; (* i.e. 0b111111 if capacity = 0b1000000 *)
       head = Atomic.make 0 ;
       tail = Atomic.make 0 ;
-      blockmask = blocksize - 1 ; (* see [mask] *)
-      blockshift = log2 blocksize ;
     }
 
   let try_enqueue q x =
@@ -433,11 +425,11 @@ module BufferQueueAtomArrOpt : QUEUE = struct
     let rec try_enqueue () =
       let head = Atomic.get q.head in
       let index = head land q.mask in
-      let real_index = ((index land q.blockmask) lsl 3) + (index lsr q.blockshift) in
+      let real_index = 8 * index in
       let ts = Atomic.Array.get q.timestamps real_index in
       if ts = head && Atomic.compare_and_set q.head head (head+1) then begin
         (* cell is available and we got it first: write our value *)
-        q.elements.(index) <- x ;
+        q.elements.(real_index) <- x ;
         Atomic.Array.set q.timestamps real_index (head+1) ;
         true
       end
@@ -461,11 +453,11 @@ module BufferQueueAtomArrOpt : QUEUE = struct
     let rec try_dequeue () =
       let tail = Atomic.get q.tail in
       let index = tail land q.mask in
-      let real_index = 8 * (index land q.blockmask) + index lsr q.blockshift in
+      let real_index = 8 * index in
       let ts = Atomic.Array.get q.timestamps real_index in
       if ts = tail+1 && Atomic.compare_and_set q.tail tail (tail+1) then begin
         (* cell is available and we got it first: read its value *)
-        let x = q.elements.(index) in
+        let x = q.elements.(real_index) in
         (*! cell.element <- dummy ; !*) (* TODO for garbage collection *)
         Atomic.Array.set q.timestamps real_index (tail + q.mask+1) ;
         Some x
