d04a7ed8291f4686e137ca3241ce59e23649409e
Olivier Nicole <olivier@chnik.fr>
2022-02-17 15:34:48 +0100

    Fix bug and improve printing

--- array_accesses.ml
+++ array_accesses.ml
@@ -7,9 +7,10 @@ let run_timed s arr_size f =
   let start = Sys.time () in
   f arr_size;
   let duration = Sys.time() -. start in
-  let duration_micros = duration *. 1000000.0 in
+  let duration_micros = duration *. 1_000_000. in
+  let duration_millis = duration *. 1_000. in
   let duration_per_element = duration_micros /. (float_of_int arr_size) in
-  Printf.printf "%s %d %d %d\n%!" s arr_size (int_of_float duration_micros) (int_of_float duration_per_element)
+  Printf.printf "%s\t%d\t%11.3f\t%d\n%!" s arr_size duration_millis (int_of_float duration_per_element)
 
 let atomic_array_sequential arr_size =
   let arr = Array.make arr_size 0 in
@@ -22,7 +23,7 @@ let atomic_array_sequential arr_size =
   done
 
 let atomic_arrayt_sequential arr_size =
-  let arr = Array.make arr_size (Atomic.make 0) in
+  let arr = Array.init arr_size (fun _ -> Atomic.make 0) in
   Gc.minor ();
   for _ = 0 to iterations-1 do
     for i = 0 to arr_size-1 do
@@ -44,7 +45,7 @@ let atomic_array_random arr_size =
   done
 
 let atomic_arrayt_random arr_size =
-  let arr = Array.make arr_size (Atomic.make 0) in
+  let arr = Array.init arr_size (fun _ -> Atomic.make 0) in
   let arr_idx = Array.init arr_size (fun _ -> Random.int arr_size) in
   Gc.minor ();
   for _ = 0 to iterations-1 do
@@ -59,12 +60,10 @@ let atomic_arrayt_random arr_size =
 
 let () =
   Random.self_init ();
-  for _ = 1 to 10 do
+  for _ = 1 to 9 do
     array_size := !array_size * 2;
-    (*
-    run_timed "atomic_array_sequential" !array_size atomic_array_sequential;
+    run_timed "atomic_array_sequential " !array_size atomic_array_sequential;
     run_timed "atomic_arrayt_sequential" !array_size atomic_arrayt_sequential;
-    *)
-    run_timed "atomic_array_random" !array_size atomic_array_random;
-    run_timed "atomic_arrayt_random" !array_size atomic_arrayt_random;
+    run_timed "atomic_array_random     " !array_size atomic_array_random;
+    run_timed "atomic_arrayt_random    " !array_size atomic_arrayt_random;
   done
